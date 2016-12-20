95e29a782feec4260d59b695b2012d26d5fa8918
upisfree <upisfree@outlook.com>
2016-12-21 01:31:13 +0600

    FIRST UNITY EXPERIENCE))0

---
+++ Assets/Fire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+  public static bool IsPlayerNearFire = false;
+
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  private void OnTriggerExit(Collider other)
+  {
+    IsPlayerNearFire = false;
+  }
+
+  private void OnTriggerStay(Collider other)
+  {
+    IsPlayerNearFire = true;
+  }
+}
---
+++ Assets/Sniper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : MonoBehaviour {
+  AudioSource audio;
+  AudioClip clip;
+
+  public static bool IsPlayerOnSniperPlane = false;
+
+  void Start() {
+    audio = gameObject.AddComponent<AudioSource>();
+    audio.volume = 1;
+    clip = Resources.Load<AudioClip>("gun18");
+    
+   InvokeRepeating("RepeatingFunction", 0, 3);
+  }
+
+  void Update() {
+  
+  }
+
+  void RepeatingFunction()
+  {
+    if (IsPlayerOnSniperPlane)
+    {
+      if (Random.Range(0, 5) == 0)
+      {
+        audio.PlayOneShot(clip, 1.0F);
+
+        if (Random.Range(0, 4) == 0)
+        {
+          IsPlayerOnSniperPlane = false;
+
+          Application.LoadLevel(0);
+        }
+      }
+    }
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    IsPlayerOnSniperPlane = false;
+  }
+
+  private void OnTriggerStay(Collider other)
+  {
+    IsPlayerOnSniperPlane = true;
+  }
+}
